fd8805e907ef3f096b9347007dedc35fdb6589a7
Benjamin Martinis <martinisbenjamin@gmail.com>
2024-01-19 16:36:57 -0600

    Same

--- app/error.tsx
+++ app/error.tsx
@@ -9,7 +9,7 @@ interface ErrorProps {
 
 export default function error({ error, reset }: ErrorProps) {
     return (
-        <main className="py-20 flex flex-col items-center justify-center">
+        <main className="py-20 px-3 flex flex-col items-center justify-center">
             <h2 className="text-4xl sm:text-6xl font-medium">Oh No!</h2>
             <p className="mt-2 sm:text-xl">{error.message}</p>
             <Link className='mt-4 sm:text-xl text-red-600 hover:text-red-500' href="/">Return Home &rarr;</Link>
