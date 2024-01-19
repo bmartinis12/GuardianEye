6346173707aa1d6c0cafe4be5346008b9b635201
Benjamin Martinis <martinisbenjamin@gmail.com>
2024-01-19 16:36:32 -0600

    error page

--- app/error.tsx
+++ app/error.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import Link from "next/link"
+
 interface ErrorProps {
     error: any,
     reset: () => void
@@ -10,7 +12,7 @@ export default function error({ error, reset }: ErrorProps) {
         <main className="py-20 flex flex-col items-center justify-center">
             <h2 className="text-4xl sm:text-6xl font-medium">Oh No!</h2>
             <p className="mt-2 sm:text-xl">{error.message}</p>
-            <p className='mt-4 sm:text-xl text-red-600 hover:text-red-500' onClick={reset}>Reset &rarr;</p>
+            <Link className='mt-4 sm:text-xl text-red-600 hover:text-red-500' href="/">Return Home &rarr;</Link>
         </main>
     )
 }
